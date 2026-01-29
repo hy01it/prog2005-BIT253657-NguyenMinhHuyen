e0d586aea6a0d309f61a056da055e8e01aac9589
hy01it <huyenn5200@gmail.com>
2026-01-29 22:14:37 +0700

    Add basic arithmetic operations in Ex3.py

---
+++ chapter1/Ex3.py
@@ -0,0 +1,11 @@
+a = int(input("Nhap so 1: "))
+b = int(input("Nhap so 2: "))
+
+print("Tổng:", a + b)
+print("Hiệu:", a - b)
+print("Tích:", a * b)
+
+if b != 0:
+    print("Thuong:", a / b)
+else:
+    print("Khong the chia cho 0")
